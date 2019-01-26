383d31125008a06493bdd05af7f0fb9972f6d88a
fu731033719 <15706804793@163.com>
2019-01-26 14:49:40 +0800

    组件内部通信

--- src/TodoList.js
+++ src/TodoList.js
@@ -6,25 +6,39 @@ class TodoList extends Component {
             list: [
                 'learn english',
                 'learn react'
-            ]
+            ],
+            inputValue: ''
         }
     }
     handleButtonClick () {
         this.setState({
-            list: [...this.state.list, 'learn something']
+            list: [...this.state.list, this.state.inputValue],
+            inputValue: ''
         })
+    }
+    handleInputChange (e) {
+        this.setState({
+            inputValue: e.target.value
+        });
+    }
+    handleDeletItem (index) {
+        const list = [...this.state.list];
+        list.splice(index, 1);
+        this.setState({
+            list
+        });
     }
     render() {
         return  (
             <div>
                 <div>
-                    <input />
+                    <input value={this.state.inputValue} onChange={this.handleInputChange.bind(this)}/>
                     <button onClick={this.handleButtonClick.bind(this)}>add</button>
                 </div>
                 <ul>
                     {
                         this.state.list.map((item, index) => {
-                            return <li key={index}>{item}</li>
+                            return <li key={index} onClick={this.handleDeletItem.bind(this,index)}>{item}</li>
                         })
                     }
                 </ul>
